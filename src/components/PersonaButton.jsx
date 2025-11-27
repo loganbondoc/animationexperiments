7e9a8f18b52e4d3f094042389126afadd5419cf0
Logan Bondoc <loganbondoc@gmail.com>
2025-11-28 00:09:33 +1100

    fixed folder structure

--- src/components/PersonaButton.jsx
+++ src/components/PersonaButton.jsx
@@ -6,15 +6,18 @@ export default function PersonaButton() {
   // useEffect means that the code will run after the component is mounted
   useEffect(() => {
     const outline1 = document.querySelector('#outline1');
-    const outline2 = document.querySelector('#outline2');
 
-    animate("#outline1 path", {
-      d: svg.morphTo("#outline2 path"),
-      duration: 30000,
-      easing: "easeInOutQuad",
-      loop: true,
-      direction: "alternate",
+    // Run animation only when mouse enters #outline1
+    outline1.addEventListener("mouseenter", () => {
+      animate("#outline1 path", {
+        d: svg.morphTo("#outline2 path"),
+        duration: 3000,
+        easing: "easeInOutQuad",
+        loop: true,
+        direction: "alternate",
+      });
     });
+
   }, []);
 
   return (
@@ -23,8 +26,8 @@ export default function PersonaButton() {
         <path d="M1018.5 19L0 0L118.798 179.5L944.063 172.246L1018.5 19Z" fill="#FF0107" />
       </svg>
 
-      <svg id="outline2" width="859" height="183" viewBox="0 0 859 183" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M858.266 27L38.2656 0L0.000488281 182.848L734.266 164.5L858.266 27Z" fill="#FF0107" style={{ display: "none" }} />
+      <svg id="outline2" width="859" height="183" viewBox="0 0 859 183" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ display: "none" }}>
+        <path d="M858.266 27L38.2656 0L0.000488281 182.848L734.266 164.5L858.266 27Z" fill="#FF0107" />
       </svg>
 
 
